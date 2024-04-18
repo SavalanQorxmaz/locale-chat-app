6e5e9375de83cde970bc59c3165f9a6c94d7cd8b
SavalanQorxmaz <savalan.qorxmaz@gmail.com>
2024-04-18 16:56:17 +0400

    first step

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server,{
+    cors: {
+      origin: "http://localhost:3000"
+    }
+  });
 
-const {createServer} = require('http');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
+io.on('connection', function (socket) {
+    socket.emit('serverEmit', 'foo', 'bar', 'baz');
+  
+    socket.on('clientEmit', function (data) {
+      console.log(data);
+    });
+  });
 
-const server = new createServer(app);
-
-
-server.listen(8000, ()=>{
-    console.log('http://localhost:8000');
-})
+server.listen(8000, () => {
+  console.log('listening on *:8000');
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { socket } from './socket';
 
 function App() {
+
+  const connectSocketF = ()=>{
+    socket.on('connect', function () {
+      socket.emit('clientEmit', 'foo', 'bar', 'baz');
+    });
+  
+    socket.on('serverEmit', function (data) {
+      console.log(data);
+    });
+  }
+
+  useEffect(()=>{
+connectSocketF()
+
+
+  },[])
+
   return (
     <div className="App">
-      frontend
+     <button onClick={connectSocketF}>send</button>
     </div>
   );
 }
