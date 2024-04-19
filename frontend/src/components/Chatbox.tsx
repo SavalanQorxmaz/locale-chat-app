368f877e8ddddfc3b1add9546c8ccd939fdce696
Savalan Tariverdiyev <savalan.t@atmu.edu>
2024-04-19 17:32:49 +0400

    chatbox

--- frontend/src/components/Chatbox.tsx
+++ frontend/src/components/Chatbox.tsx
@@ -2,7 +2,38 @@ import React from 'react'
 
 const Chatbox = () => {
   return (
-    <div>Chatbox</div>
+    <div className='container'>
+      <div className='generalBox'>
+        <div className='nameLine'>
+          <p>
+            room
+          </p>
+
+        </div>
+        <div className='conversationBox'>
+
+        </div>
+        <div className='typingLine'>
+          <input type="text" name="" id="" className='typing'/>
+       
+          <div className='sendTyping'>
+          &#10548;
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className='loginPage'>
+        <form action="">
+        <input type="text" placeholder='nickname' id='name'/>
+        <input type="password" placeholder='password' id='password'/>
+        <input type="submit" id='submit'/>
+        </form>
+      </div>
+
+
+    </div>
   )
 }
 
