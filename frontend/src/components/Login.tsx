9eaa8d61efce8d0fb601edc2a442c408bcdfd86c
Savalan Tariverdiyev <savalan.t@atmu.edu>
2024-04-22 17:07:04 +0400

    container alinmir)))

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -3,9 +3,14 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='loginPage'>
+      <h3>Söhbətə başla</h3>
     <form action="">
-    <input type="text" placeholder='nickname' id='name'/>
-    <input type="password" placeholder='password' id='password'/>
+      <div className='selectRoom'>
+      <input type="text" placeholder='room' id='room'/>
+      <input type="radio" id='radio'/>
+    <input type="text" placeholder='create room' id='createRoom'/>
+      </div>
+    <input type="text" placeholder='nickname' id='nickname'/>
     <input type="submit" id='submit'/>
     </form>
   </div>
